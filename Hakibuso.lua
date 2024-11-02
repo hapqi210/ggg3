d3e9c447d4ed5bffe3a8ea198429643de4a20354
hapqi210 <157613652+hapqi210@users.noreply.github.com>
2024-11-02 16:45:12 +0700

    Add files via upload

---
+++ Hakibuso.lua
@@ -0,0 +1,13 @@
+_G.AUTOHAKI = true
+spawn(function()
+	while wait(.1) do
+		if _G.AUTOHAKI then 
+			if not game.Players.LocalPlayer.Character:FindFirstChild("HasBuso") then
+				local args = {
+					[1] = "Buso"
+				}
+				game:GetService("ReplicatedStorage").Remotes.CommF_:InvokeServer(unpack(args))
+			end
+		end
+	end
+end)
